5162026f8934b636b0a7c437134720f9d5fcd8a8
Jeppe <Jeppe@dhcp-076121.eduroam.chalmers.se>
2015-02-18 16:02:31 +0100

    Added MenuController, MenuRendererModel and MenuRenderView

---
+++ src/MenuController.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+
+public class MenuController
+{
+	private MenuRenderModel menuModel; 
+	private MenuRenderView menuView; 
+	public MenuController(int screenResolutionWidth,int screenResolutionHeight)
+	{
+		menuModel = new MenuRenderModel();
+		//menuView = new MenuRenderView(menuModel, screenResolutionWidth, screenResolutionHeight);
+	}
+	
+	public MenuRenderView getView()
+	{
+		return menuView;
+	}
+}
